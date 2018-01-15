2faad1fbbd7633dd0c2172338b37df856c1558f2
ilumos <ilumos@gmail.com>
2018-01-15 19:59:17 +0000

    Adding basic install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Exit if there is an error
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# If script is executed as an unprivileged user
+# Execute it as superuser, preserving environment variables
+if [ $EUID != 0 ]; then
+    sudo -E "$0" "$@"
+    exit $?
+fi
+
+# If there is an .env file use it
+# to set the variables
+if [ -f $SCRIPT_DIR/.env ]; then
+    source $SCRIPT_DIR/.env
+fi
+
+# Add elastic apt repo if it does not already exist
+if [[ ! -f /etc/apt/sources.list.d/elastic-6.x.list ]]; then
+    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+fi
+
+# Install required packages
+/usr/bin/apt update -y
+/usr/bin/apt install -y elasticsearch \
+                        kibana \
+                        default-jre
+
+# Load the new service file
+/bin/systemctl daemon-reload
+
+# Set services to start at boot
+/bin/systemctl enable elasticsearch
+/bin/systemctl enable kibana
+
+# Start the services
+/bin/systemctl start elasticsearch
+/bin/systemctl start kibana
