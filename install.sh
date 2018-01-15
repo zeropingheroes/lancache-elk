595bfd295d5a6d5351847105e6e31d943accabac
ilumos <ilumos@gmail.com>
2018-01-15 20:26:53 +0000

    Adding LetsEncrypt certificate acquisition via DigitalOcean

--- install.sh
+++ install.sh
@@ -18,24 +18,53 @@ if [ -f $SCRIPT_DIR/.env ]; then
     source $SCRIPT_DIR/.env
 fi
 
+# Check all required variables are set
+: "${DO_APIKEY:?must be set}"
+: "${DO_DOMAIN:?must be set}"
+: "${DO_EMAIL:?must be set}"
+
 # Add elastic apt repo if it does not already exist
 if [[ ! -f /etc/apt/sources.list.d/elastic-6.x.list ]]; then
     echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
 fi
 
 # Install required packages
+/usr/bin/add-apt-repository -y ppa:certbot/certbot
 /usr/bin/apt update -y
 /usr/bin/apt install -y elasticsearch \
+                        logstash \
                         kibana \
-                        default-jre
+                        default-jre \
+                        python-pip \
+                        certbot
+
+# Upgrade Python-Pip
+/usr/local/bin/pip install --upgrade pip
+
+# Get letsencrypt certificate
+/usr/bin/certbot certonly --manual \
+    -m ${DO_EMAIL} \
+    --agree-tos \
+    -n \
+    --manual-public-ip-logging-ok \
+    -d elk.lan.zeropingheroes.co.uk \
+    --preferred-challenges dns \
+    --manual-auth-hook lets-do-dns \
+    --manual-cleanup-hook lets-do-dns
+
+# Start the certbot timer (cron)
+/bin/systemctl enable certbot.timer
+/bin/systemctl start certbot.timer
 
 # Load the new service file
 /bin/systemctl daemon-reload
 
 # Set services to start at boot
 /bin/systemctl enable elasticsearch
+/bin/systemctl enable logstash
 /bin/systemctl enable kibana
 
 # Start the services
 /bin/systemctl start elasticsearch
+/bin/systemctl start logstash
 /bin/systemctl start kibana
